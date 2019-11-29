7359c3d6a64670281388fc5cde271cd3447547a4
Anthony Alcouffe <anthonyalcouffe@ynov.com>
2019-11-29 16:49:10 +0100

    Add exo yeux

--- Diabete/main.py
+++ Diabete/main.py
@@ -1,4 +1,11 @@
-
+from pyDatalog.pyDatalog import create_terms, ask, load, assert_fact, clear
 
 if __name__ == '__main__':
-    
+    clear()
+    create_terms('X, age, glycemie, concerned, diabete')
+
+    load("""
+            concerned(X) <= (age(X) <= 5)
+            hyperglycemic_risk(X) <= concerned(X) & (glycemie(X) > 10)
+            hyperglycemic_risk(X) <= concerned(X) & (glycemie(X) < 5)
+        """)
